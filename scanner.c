7c8bfa8fc1e23f1294fb6a67ca573535dedeb994
john rinderer <jrinder@jrinder.localdomain>
2021-05-15 15:58:55 -0500

    moved the string builder to universal functions, and added a function to free any string that's been created

--- scanner.c
+++ scanner.c
@@ -6,27 +6,11 @@
 #include "global_defs.h"
 
 
-void stringBuilder(char **strng, char c){
-    int size=0;
-    int i=0;
-
-    char *temp_pntr = (char*)malloc(size+1 * sizeof(char));
-    temp_pntr = strng;
-    int size2 = strlen(strng);
-    temp_pntr[size2]=c;
-
-    strng = realloc(strng,size+100);
-    strng = temp_pntr;
-    //return strng;
-
-
-}
-
 //we pass the source code file after main validates there are no unusual or invalid characters
 void start_scanner(FILE *f){
     char *running_str = (char*)malloc(0 * sizeof(char));
     char current_c = 'c';
-    stringBuilder(running_str,current_c);
+    stringBuilder(running_str,current_c); //this function is in universal_funcs.h
     current_c = 'a';
     stringBuilder(running_str,current_c);
     current_c = 'k';
@@ -34,6 +18,7 @@ void start_scanner(FILE *f){
     current_c = 'e';
     stringBuilder(running_str,current_c);
     printf("The current string is:%s",running_str);
-
+    
+    freeStrings(running_str); //at the end of the scanner free any memory used by string
 
 }
--- universal_funcs.h
+++ universal_funcs.h
@@ -17,4 +17,24 @@ int isExAcceptableChar(char c) {
         return 0;
 }
 
+void stringBuilder(char **strng, char c){
+    int size=0;
+    int i=0;
+
+    char *temp_pntr = (char*)malloc(size+1 * sizeof(char));
+    temp_pntr = strng;
+    int size2 = strlen(strng);
+    temp_pntr[size2]=c;
+
+    strng = realloc(strng,size+1);
+    strng = temp_pntr;
+    //return strng;
+
+
+}
+
+void freeStrings(char **strng){
+    free(strng);
+}
+
 #endif //LUSCC_UNIVERSAL_FUNCS_H
