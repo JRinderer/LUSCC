51e424d3b56208a08815cbd74ffd3b82022f55d2
john rinderer <jrinder@jrinder.localdomain>
2021-05-15 08:00:37 -0500

    added code in IDE. Nothing running right now

---
+++ cmake-build-debug/CMakeFiles/LUSCC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LUSCC.dir/main.c.o"
+  "LUSCC"
+  "LUSCC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LUSCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LUSCC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LUSCC.
+# This may be replaced when dependencies are built.
